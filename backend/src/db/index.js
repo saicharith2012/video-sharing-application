e2548805adfa62c6c51c0325f91623e3fcddd9bc
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-03-07 00:26:06 +0530

    connection to database

---
+++ backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `MongoDB is connected! DB Hosted at ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB Connection FAILED !!! ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
