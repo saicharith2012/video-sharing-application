5f16cebeb1555449bc005bf117be110dd4130511
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-03-07 15:20:52 +0530

    created express app and utils

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,21 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "../env",
 });
 
-connectDB();
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection FAILED!!!", error)
+})
 
 
 
@@ -33,6 +41,7 @@ connectDB();
 // import express from "express";
 // const app = express();
 
+// // IIFE
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
